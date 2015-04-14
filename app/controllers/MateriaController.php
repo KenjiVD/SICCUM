423fbefc8dc8dbc60384ced528ef3f756ac3afe6
KenjiVD <idsemanuelupch@gmail.com>
2015-04-14 17:34:30 -0500

    Update MateriaController.php

--- app/controllers/MateriaController.php
+++ app/controllers/MateriaController.php
@@ -1,6 +1,8 @@
 <?php
 
 class MateriaController extends BaseController {
+	/*Esta funcion sirve para crear una nueva materia en la base de datos, con base al llenado de un 
+	formulario previo, una vez que realiza la operacion redirige a la pagina de inicio del sistema*/
 	public function Nuevo(){
 		DB::table("materia")->insertGetId(array(
 			"periodo_idperiodo" => $_POST["periodo"],
