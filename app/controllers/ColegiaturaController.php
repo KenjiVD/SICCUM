2cfc7e141b4bdee74dbccc3350d1600de734bf90
KenjiVD <idsemanuelupch@gmail.com>
2015-04-14 17:33:01 -0500

    Update ColegiaturaController.php

--- app/controllers/ColegiaturaController.php
+++ app/controllers/ColegiaturaController.php
@@ -1,6 +1,8 @@
 <?php
 
 class ColegiaturaController extends BaseController {
+	/*Esta funcion sirve para crear una nueva colegiatura para un alumno en la base de datos, con base al llenado de un 
+	formulario previo, una vez que realiza la operacion redirige a la pagina de inicio del sistema*/
 	public function Nuevo(){
 		$CoCalumno = DB::table("alumno")->where('matriculaa', $_POST["matricula"])->first();
 
